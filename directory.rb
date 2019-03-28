56cb3ec2a01ea87acba0f91e7b988c7e8d8c123b
callum-marshall <callum_marshall@hotmail.co.uk>
2019-03-28 09:50:38 +0000

    more exercises - 2

--- directory.rb
+++ directory.rb
@@ -84,15 +84,8 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  filename = ARGV.first# first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
-  end
+  load_students("students.csv")
+  puts "Loaded #{@students.count} from students.csv"
 end
 
 def add_student(name, cohort)
