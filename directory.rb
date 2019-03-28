d0f6e46ce636c4a7a8d797c18a8ae1fdd3b424f0
callum-marshall <callum_marshall@hotmail.co.uk>
2019-03-28 15:24:13 +0000

    more exercises - 7

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require 'csv'
 @students = []
 
 def try_load_students
@@ -71,22 +72,18 @@ end
 def save_students
   puts "Tell me in which file to save:"
   filename = gets.chomp
-  file = File.open(filename, "w") do |file|
+  CSV.open(filename, "w") do |csv|
     @students.each do |student|
-      student_data = [student[:name], student[:cohort]]
-      csv_line = student_data.join(",")
-      file.puts csv_line
+      csv << [student[:name], student[:cohort]]
     end
   end
   puts "Saved #{@students.count} students to '#{filename}'"
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r") do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(',')
+  CSV.foreach(filename) do |row|
+      name, cohort = row
       add_student(name)
-    end
   end
   puts "Loaded #{@students.count} students from '#{filename}'"
 end
